c6b535e3a90447aa956f0809fc1acea3c4db017c
mysoyul <vega2k_@naver.com>
2024-02-07 11:48:50 +0900

    List update

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -79,7 +79,7 @@
 </template>
 
 <script setup>
-import { ref, reactive, watch, computed, onMounted } from 'vue';
+import { reactive, watch, computed, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useUsersStore } from '@/stores';
 
